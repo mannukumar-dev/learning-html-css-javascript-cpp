c7922e05dfb9159b07f4ee79412232af85bb44d1
Mannu Kumar <mannukumar.codes@gmail.com>
2026-08-31 18:45:49 +0530

    solved the problem 03_number_ques.cpp

---
+++ cpp-learning/questions/03_number_ques.cpp
@@ -0,0 +1,158 @@
+// 🟡 Level 3 — Number Logic
+
+// These are very important for college + programming fundamentals.
+
+// Check whether a number is a palindrome.
+// Input: 121
+// Output: Palindrome
+// Check whether a number is prime.
+// Print all prime numbers from 1 to N. //doubts
+// Find the largest digit in a number.
+// Input: 58392
+// Output: 9
+// Find the smallest digit. 
+// Count how many times a particular digit occurs.
+// Input: 1223342
+// Digit: 2
+// Output: 3 //doubts
+
+// Check Armstrong number.
+// 153 → Armstrong
+// Print Armstrong numbers from 1 to N.
+// Find GCD of two numbers.
+// Find LCM of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // question 1: Check whether a number is a palindrome.
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+    int original_num = num;
+    int reverse = 0;
+    int last_digit;
+    // while (num > 0)
+    // {
+    //     last_digit = num%10;
+    //     reverse = reverse*10 + last_digit;
+    //     num /= 10;
+
+    // }
+
+    // if (original_num == reverse)
+    // {
+    //     cout<<"Palindrome";
+    // }
+    // else
+    // {
+    //     cout<<"Not a Palindrome";
+    // }
+
+    // question 2: Check whether the number is prime or not.
+    // bool isPrime = true;
+    
+
+    // if (num < 0)
+    // {
+    //     isPrime = false;
+    // }
+    // else
+    // {
+    //     for (int i = 2; i < num; i++)
+    //     {
+    //         if (num % i == 0)
+    //         {
+    //             isPrime = false;
+    //             break;
+    //         }
+
+    //     }
+        
+    // }
+
+    // if (isPrime)
+    // {
+    //     cout << "Prime";
+    // }
+    // else
+    // {
+    //     cout << "Not a prime";
+    // }
+
+    
+    //question 3:  Print all prime numbers from 1 to N.
+
+    // for (int i = 2; i <= num; i++)
+    // {
+    //     bool isPrime = true;
+    //     for (int j = 2; j < i; j++)
+    //     {
+    //         if (i % j == 0)
+    //         {
+    //             isPrime = false;
+    //             break;
+    //         }
+            
+    //     }
+    //     if (isPrime)
+    //     {
+    //         cout<<i<<" ";
+    //     }
+        
+        
+    // }
+    
+
+
+
+
+    //question 4: Find the largest digit in a number.
+
+    // int largest_digit = INT16_MIN;
+    // int digit;
+    // while (num > 0)
+    // {
+    //     digit = num%10;
+    //     if (digit > largest_digit)
+    //     {
+    //         largest_digit = digit;
+    //     }
+        
+    //     num /= 10;
+    // }
+
+    // cout<<largest_digit<<endl;
+
+
+    // question 5: Find the smallest digit.
+    // int smallest_digit = INT16_MAX;
+
+    // while (num > 0)
+    // {
+    //     digit = num%10;
+    //     if (digit < smallest_digit)
+    //     {
+    //         smallest_digit = digit;
+    //     }
+        
+    //     num /= 10;
+    // }
+
+    // cout<<smallest_digit<<endl;
+
+
+    //question 6: Count how many times a particular digit occurs.
+    // Input: 1223342
+    // Digit: 2  doubts
+
+
+
+
+    
+
+    return 0;
+}
--- cpp-learning/questions/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    //question 1: Print numbers from 1 to N
-    int N;
-    cout<<"Enter the number: ";
-    cin>>N;
